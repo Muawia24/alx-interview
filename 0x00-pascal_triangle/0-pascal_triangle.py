a5db830ef067cbfe81fd82d60864999f9e49ddee
Ahmed Muawia <amuawia666@gmail.com>
2024-10-03 19:59:50 +0300

    Pycodestyle fixation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """ 0-pascal_triangle.py """
+
+
 def pascal_triangle(n):
     """
      returns a list of lists of integers representing
      the Pascal's triangle of n
     """
     if n <= 0:
-        return []
+        return list()
+    # Creates triangle rows without the values
     triangle = [[1 for j in range(i)] for i in range(1, n + 1)]
+    # Triangle traversal
     for row in range(2, len(triangle)):
         for column in range(1, len(triangle[row])):
             if column + 1 < len(triangle[row]):
-                triangle[row][column] = triangle[row - 1][column - 1] + triangle[row - 1][column]
+                # Calculate row values
+                triangle[row][column] = triangle[row - 1][column - 1] + \
+                                    triangle[row - 1][column]
 
     return triangle
-
