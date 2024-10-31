6cb9df04fbbf4f361ce5c4e068ab5e8a043a4c18
Ahmed Muawia <amuawia666@gmail.com>
2024-11-01 01:55:37 +0200

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+ UTF-8 Validation
+"""
+
+
+def check(arr):
+    for x in arr:
+        b = format(x, "08b")
+        if b[:2] != "10":
+            return False
+    return True
+
+
+def validUTF8(data):
+    for i in range(len(data)):
+        if isinstance(data[i], int) and data[i] > 244 or data[i] < 0:
+            return False
+        b = format(data[i], "08b")
+        if b[0] == 0:
+            continue
+        if b[:3] == "110":
+            if i >= (len(data) - 1):
+                return False
+            if not check(list((data[i + 1],))):
+                return False
+            continue
+
+        if b[:4] == "1110":
+            if i >= (len(data) - 2):
+                return False
+            if not check(list((data[i + 1], data[i + 2]))):
+                return False
+            continue
+
+        if b[:5] == "11110":
+
+            if i >= (len(data) - 3):
+                return false
+            if not check(list((data[i + 1], data[i + 2], data[i + 3]))):
+                return False
+            continue
+
+        return True
