7160609c3757d26ebe208e1fb2909f3c14d6f4ec
Ahmed Muawia <amuawia666@gmail.com>
2024-11-02 16:11:15 +0200

    Handling empty charset

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,35 +19,34 @@ def validUTF8(data):
     """
     Validates UTF-8 characters
     """
-    for i in range(len(data)):
+    i = 0
+    while i < len(data):
         if not isinstance(data[i], int) or data[i] > 244 or data[i] < 0:
             return False
         if data[i] in [192, 193]:
             return False
 
         b = format(data[i], "08b")
-        if b[0] == 0:
-            continue
-        if b[:3] == "110":
-            if i >= (len(data) - 1):
-                return False
-            if not check(list((data[i + 1],))):
-                return False
+        if b[0] == "0":
+            i += 1
             continue
 
-        if b[:4] == "1110":
-            if i >= (len(data) - 2):
+        elif b[:3] == "110":
+            if i + 1 >= len(data) or not check([data[i + 1]]):
                 return False
-            if not check(list((data[i + 1], data[i + 2]))):
+            i += 2
+
+        elif b[:4] == "1110":
+            if i + 2 >= len(data) or not check([data[i + 1], data[i + 2]]):
                 return False
-            continue
+            i += 3
 
-        if b[:5] == "11110":
-
-            if i >= (len(data) - 3):
-                return false
-            if not check(list((data[i + 1], data[i + 2], data[i + 3]))):
+        elif b[:5] == "11110":
+            if i + 3 >= len(data) or not check(
+                    [data[i + 1], data[i + 2], data[i + 3]]):
                 return False
-            continue
+            i += 4
+        else:
+            return False
 
-        return True
+    return True
