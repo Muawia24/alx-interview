f8740f668eda502301238bb8ba6279e86adb842c
Ahmed Muawia <amuawia666@gmail.com>
2024-11-28 11:11:11 +0200

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0. Change comes from within
+"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """
+     Determines the fewest number of coins needed to
+     meet a given amount total.
+    """
+    if total <= 0 or not coins:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
